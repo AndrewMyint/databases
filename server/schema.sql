c6d3561c5d9a60a5864082fdb7f6b49cde7f4f1b
Andrew Myint <doku.potharhtoo@gmail.com>
2019-02-06 21:59:12 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,30 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT,
+  text VARCHAR(255),
+  date DATE,
+  PRIMARY KEY (id)
 );
 
+
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+  Id INT,
+  Name VARCHAR(30),
+  PRIMARY KEY (Id)
+);
 
-
-
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  Id INT,
+  roomName VARCHAR(30),
+  PRIMARY KEY (Id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
